91cad8cf85ce4c01b79d1a76d057da60eb7c821a
John Freeborn <jfx1026@gmail.com>
2019-08-19 01:19:53 -0700

    White Label Version + fixes
    Bug with SKOW is fixed! - it was a JSON issue.
    New while label, single text (no text/movie choise) version for easier grab and create.

---
+++ greekData.js
@@ -0,0 +1,107 @@
+// CHECK OUT THE SELECTED MOVIE ATTRIBUTES
+
+function movieCount() {
+  console.log(" - - - movieCount function - - - ");
+  
+  pCount = globalJsonVar[movieIndex].paragraphs.length; // I need the number of paragraphs in the movie selected
+  console.log("pCount: total number of paragraphs in selected movie = " + pCount);
+    
+  for (xC = 0; xC < globalJsonVar[movieIndex].paragraphs.length; xC++) {
+    var tC = globalJsonVar[movieIndex].paragraphs[xC].length;
+    lCount = tC + lCount;
+  }
+  
+  console.log("lCount: total number of paragraphs in selected movie = " + lCount);
+}
+
+function changeMovie() {  
+  console.log("changeMovie tiggered!!!")
+  
+  //grab text from dropdown
+  var tempM = $("#movieDropdown option:selected").text();
+  console.log("tempM = " + tempM);
+  
+  //clear text in box first
+  document.getElementById("loremIpsumBox").innerHTML = (""); // empty the div
+
+}
+
+// CSS Style Sheet swap function
+function swapStyleSheet(sheet) {
+    document.getElementById("pagestyle").setAttribute("href", sheet);  
+}
+
+// GENERATOR FUNCTIONS
+
+// write sentences to the page
+function writeSentences() {
+  
+  var tempCounter = 0; // count the number of lines read
+  console.log("start counting... " + tempCounter);
+
+  tempData = ""; // reset tempData var
+  
+  //clear text in box first
+  document.getElementById("loremIpsumBox").innerHTML = (""); // empty the div
+  
+  var rS = randomStart; // temp var to hold the random starting point
+  
+  // find lines here
+  
+  var stringData = []; //create stringData array
+  
+  //start by finding the right paragraph
+  for (var findPara = 0; tempCounter < rS; findPara++) {
+    var numTemp = globalJsonVar[movieIndex].paragraphs[findPara].length;
+    tempCounter = tempCounter + numTemp;
+    console.log("still counting... " + tempCounter);
+  }
+  //which paragraph are we on - or where should we start?
+  console.log("start on paragraph number " + findPara);
+
+  var countLines = 0; //temp var to count the lines grabbed
+  
+  for (var grab = 0; countLines < numberSelect; grab++) {
+    //add new line after each paragraph
+    if (grab !== 0) {
+      console.log("NEW LINE!!!");
+      stringData.push("\n" + " "); //add return after paragraph - NOT WORKING
+      console.log(stringData);
+    }
+    
+    for (s = 0; countLines < numberSelect; s++) {
+      tempData = (globalJsonVar[movieIndex].paragraphs[findPara+grab][s] + " ");  // grab a line from the randomStart paragraph
+      stringData.push(tempData); // add new string to array
+      countLines++;
+      
+      //check next line for 'undefined'
+      //console.log("Check next line...");
+      //console.log(globalJsonVar[movieIndex].paragraphs[findPara+grab][s+1]);
+      if (typeof (globalJsonVar[movieIndex].paragraphs[findPara+grab][s+1]) == 'undefined') {
+        console.log("Next line is blank!!!");
+        break;
+      }
+      
+    }
+  }
+
+  stringData = stringData.join(" "); // remove the commas from the output
+  document.getElementById("loremIpsumBox").innerHTML += ("<p>" + stringData + "</p>"); // puts the words in the div
+  
+}
+
+// write text to page based on user vars
+function writeParagraphs() {
+  //clear text in box first
+  document.getElementById("loremIpsumBox").innerHTML = (""); // empty the div
+  
+  var rP = randomStart; // temp var to hold the random starting point
+
+  for (var x = 0; x < numberSelect; x++) {
+    var tempData = globalJsonVar[movieIndex].paragraphs[rP];
+    console.log ("rP = " + rP); // testing random variable
+    tempData = tempData.join(" "); // remove the commas from the output          
+    document.getElementById("loremIpsumBox").innerHTML += ("<p>" + tempData + "</p>"); // puts the words in the div
+    rP++; //increment rP to keep the loop going
+  }
+}
---
+++ lorem-ispum-min.js
@@ -0,0 +1,89 @@
+// VARIABLES
+
+// create global variable for JSON data
+var globalJsonVar;
+      
+// initialize variables
+var randomStart = 0; // initialize the randomStart variable
+var tempLimit = 0;
+var movieIndex = 0; // indexByName(movieSelect) - an attempt to make this more dynamic
+var stringData = new Array(); // create array for lines
+
+// user variables
+var movieSelect = "Pretty In Pink"; // using this movie for testing
+var unitSelect = "paragraphs"; // usuing this for testing - could be paragraphs or lines
+var numberSelect = 2; // using 10 for testing
+var pCount = 0 // how many paragraphs in the movie selected
+var lCount = 0; // how many lines in the movie selected
+
+// FUNCTIONS
+
+// function to grab the JSON and create the array  
+function grabJSON() {
+  $.getJSON("greek-text.json", function(json) {
+    globalJsonVar = json; // this creates the json data object inside the globalJsonVar
+    loadMovieData(); // trigger function to load the movie data
+  });
+}
+
+// function to load the movie data into an array 
+function loadMovieData() {
+  console.log("start loading movie data");
+  console.log("movie data loaded - see array below");
+  console.log(globalJsonVar);
+
+  //start movieCount function
+  movieCount();
+}
+
+//create a random starting point, based on movie, type and number of units requested then start the function to write the text to the 
+function getRange() {
+  
+  //start changeMovie function to grab movieSelect index
+  changeMovie();
+  
+  //get the number from user input field
+  numberSelect = document.getElementById("numOption").value;
+  console.log("numberSelect = " + numberSelect);
+
+  //make numberSelect a number - not a string
+  numberSelect = parseInt(numberSelect, 10);
+  console.log("numberSelect after parseInt = " + numberSelect);
+  
+  //get a random starting point
+  randomizer();
+        
+  //put the words on the screen
+  if (unitSelect == "paragraphs") {
+    writeParagraphs(); // start paragraphs function
+  } else {
+    writeSentences(); // start sentences function
+  }
+}
+
+function randomizer() {
+  // for paragraphs
+  if (unitSelect == "paragraphs") {
+
+    randomStart = Math.floor((Math.random() * (pCount - numberSelect))); // generate random number between 0 and max units
+    console.log ("paragraph randomStart = " + randomStart);
+  
+  // for sentences
+  } else {
+    console.log("lCount = " + lCount);
+    randomStart = Math.floor((Math.random() * (lCount - numberSelect)));
+    console.log ("sentence randomStart = " + randomStart);
+  }
+
+}
+
+//set the type of output, sentences or paragraphs
+function setUnits(u) {
+  unitSelect = u;
+  console.log("Paragraphs or Sentences? --> " + unitSelect);
+}
+
+//START THE PROCESS
+
+// get the data
+grabJSON();
